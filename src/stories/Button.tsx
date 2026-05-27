4557816ec08434c9c025633b5283a1aeee071f0d
DCampbellCreative <dougcampbell1990@gmail.com>
2026-05-26 22:21:52 -0400

    add basic button and css vars

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,37 +1,62 @@
-import React from 'react';
+// import React from 'react';
 
-import './button.css';
+// import './button.css';
+
+// export interface ButtonProps {
+//   /** Is this the principal call to action on the page? */
+//   primary?: boolean;
+//   /** What background color to use */
+//   backgroundColor?: string;
+//   /** How large should the button be? */
+//   size?: 'small' | 'medium' | 'large';
+//   /** Button contents */
+//   label: string;
+//   /** Optional click handler */
+//   onClick?: () => void;
+// }
+
+// /** Primary UI component for user interaction */
+// export const Button = ({
+//   primary = false,
+//   size = 'medium',
+//   backgroundColor,
+//   label,
+//   ...props
+// }: ButtonProps) => {
+//   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+//   return (
+//     <button
+//       type="button"
+//       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+//       style={{ backgroundColor }}
+//       {...props}
+//     >
+//       {label}
+//     </button>
+//   );
+// };
+
+import styles from "./button.module.css";
+import '../index.css';
 
 export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
-  primary?: boolean;
-  /** What background color to use */
-  backgroundColor?: string;
-  /** How large should the button be? */
-  size?: 'small' | 'medium' | 'large';
-  /** Button contents */
   label: string;
-  /** Optional click handler */
+  href?: string;
   onClick?: () => void;
-}
+};
 
-/** Primary UI component for user interaction */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({ label, href, onClick }: ButtonProps) => {
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
+    <a
+      className={styles.button}
+      onClick={onClick}
+      href={href}
+      rel='noopener noreferrer'
+      target='_blank'
     >
-      {label}
-    </button>
+      <p className={styles.text}>{label}</p>
+    </a>
   );
 };
+
+// export default Button;
